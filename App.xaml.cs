5b5536de6bc65a51f5c25ce0f72edbf6977ce98a
Ruben Darío Rueda <ingeniero.rubenrueda@gmail.com>
2024-01-30 12:15:38 -0500

    FirstCommit
    Implementation on One Excercise with custom visualState

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EstadosVisualesPersonalizados
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EstadosVisualesPersonalizados
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            GoToState(false);
+        }
+
+        private void entrada_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool esValido = Regex.IsMatch(e.NewTextValue, @"^[2-9]\d{2}-\d{3}-\d{4}$");
+            GoToState(esValido);
+        }
+
+        void GoToState (bool esValido)
+        {
+            string estadoVisual = esValido ? "Valido" : "Invalido";
+            VisualStateManager.GoToState(stack, estadoVisual);
+        }
+    }
+
+}
